ea6de67aa40bdda84165550ba4379cf48aef227e
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-04-26 12:39:15 +0000

    Archetypes update

--- archetypes/vulpe-struts2-jpa-archetype/src/main/resources/archetype-resources/domains/src/main/java/org/vulpe/config/domains/package-info.java
+++ archetypes/vulpe-struts2-jpa-archetype/src/main/resources/archetype-resources/domains/src/main/java/org/vulpe/config/domains/package-info.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-@VulpeDomains(useDB4O = true)
+@VulpeDomains
 package org.vulpe.config.domains;
 
 import org.vulpe.config.annotations.VulpeDomains;
