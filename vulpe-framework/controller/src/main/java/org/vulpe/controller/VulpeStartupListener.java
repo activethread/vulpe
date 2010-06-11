16fe4576d7c799fae5a966c68ebb5aaa252c77b7
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-06-11 11:45:14 +0000

    Vulpe update

--- vulpe-framework/controller/src/main/java/org/vulpe/controller/VulpeStartupListener.java
+++ vulpe-framework/controller/src/main/java/org/vulpe/controller/VulpeStartupListener.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.vulpe.controller.listeners;
+package org.vulpe.controller;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
