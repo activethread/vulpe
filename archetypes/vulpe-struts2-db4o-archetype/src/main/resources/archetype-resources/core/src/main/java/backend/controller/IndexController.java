16647a711e8fe3a48521886c986f87a3cb036052
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-06-14 17:00:05 +0000

    Vulpe archetypes update

--- archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/core/src/main/java/backend/controller/IndexController.java
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/core/src/main/java/backend/controller/IndexController.java
@@ -13,6 +13,6 @@ import org.vulpe.controller.annotations.Controller.ControllerType;
 @Controller(controllerType = ControllerType.BACKEND)
 public class IndexController extends ApplicationBaseSimpleController {
 
-	protected static final Logger LOG = Logger.getLogger(IndexAction.class);
+	protected static final Logger LOG = Logger.getLogger(IndexController.class);
 
 }
