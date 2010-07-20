191cf2efc5f7f87c088ab9cb890845572e2bfb3c
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-07-20 19:55:46 +0000

    Vulpe archetypes update

--- archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/core/src/main/java/controller/ApplicationBaseController.java
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/core/src/main/java/controller/ApplicationBaseController.java
@@ -7,10 +7,10 @@ import java.io.Serializable;
 import org.apache.log4j.Logger;
 
 import org.vulpe.controller.struts.VulpeStrutsController;
-import org.vulpe.model.entity.VulpeBaseEntity;
+import org.vulpe.model.entity.VulpeEntity;
 
 @SuppressWarnings( { "serial", "unchecked" })
-public class ApplicationBaseController<ENTITY extends VulpeBaseEntity<ID>, ID extends Serializable & Comparable>
+public class ApplicationBaseController<ENTITY extends VulpeEntity<ID>, ID extends Serializable & Comparable>
 		extends VulpeStrutsController<ENTITY, ID> {
 	
 	protected static final Logger LOG = Logger.getLogger(ApplicationBaseController.class);
