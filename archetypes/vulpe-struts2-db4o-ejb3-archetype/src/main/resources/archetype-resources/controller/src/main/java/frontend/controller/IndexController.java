a2fa170aca5b4d1b200020140315d8766596a29b
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-07-22 17:59:34 +0000

    Vulpe archetypes new version

---
+++ archetypes/vulpe-struts2-db4o-ejb3-archetype/src/main/resources/archetype-resources/controller/src/main/java/frontend/controller/IndexController.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.frontend.controller;
+
+import org.apache.log4j.Logger;
+
+import ${package}.controller.ApplicationBaseSimpleController;
+import org.vulpe.controller.annotations.Controller;
+import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
+
+@SuppressWarnings("serial")
+@Controller(controllerType = ControllerType.FRONTEND)
+public class IndexController extends ApplicationBaseSimpleController {
+
+	protected final Logger LOG = Logger.getLogger(IndexController.class);
+
+}
