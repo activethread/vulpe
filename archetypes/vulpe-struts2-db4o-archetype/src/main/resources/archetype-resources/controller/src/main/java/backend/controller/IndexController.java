6533977edac2a7628321222386ef0874cb8428ae
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-07-22 17:57:54 +0000

    Vulpe archetypes new version

---
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/controller/src/main/java/backend/controller/IndexController.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.backend.controller;
+
+import org.apache.log4j.Logger;
+
+import ${package}.controller.ApplicationBaseSimpleController;
+import org.vulpe.controller.annotations.Controller;
+import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
+
+@SuppressWarnings("serial")
+@Controller(controllerType = ControllerType.BACKEND)
+public class IndexController extends ApplicationBaseSimpleController {
+
+	protected static final Logger LOG = Logger.getLogger(IndexController.class);
+
+}
---
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/web/src/main/webapp/WEB-INF/protected-jsp/frontend/Index/Index.jsp
@@ -0,0 +1,5 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%@include file="/WEB-INF/protected-jsp/commons/common.jsp"%>
+<h2><fmt:message key='label.${rootArtifactId}.frontend.Index.congratulations'/></h2>
