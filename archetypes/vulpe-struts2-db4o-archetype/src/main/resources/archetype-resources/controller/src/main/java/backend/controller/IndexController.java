ca0a71e91cab7d3ced7d904bd934f3f89e4aa7fc
vulpeframework <vulpeframework@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-07-27 13:18:56 +0000

    Vulpe archetypes update

--- archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/controller/src/main/java/backend/controller/IndexController.java
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/controller/src/main/java/backend/controller/IndexController.java
@@ -6,10 +6,16 @@ package ${package}.backend.controller;
 import org.apache.log4j.Logger;
 
 import ${package}.controller.ApplicationBaseSimpleController;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.vulpe.controller.annotations.Controller;
 import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
 
 @SuppressWarnings("serial")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Component("backend.IndexController")
 @Controller(controllerType = ControllerType.BACKEND)
 public class IndexController extends ApplicationBaseSimpleController {
 
--- archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/controller/src/main/java/frontend/controller/IndexController.java
+++ archetypes/vulpe-struts2-db4o-archetype/src/main/resources/archetype-resources/controller/src/main/java/frontend/controller/IndexController.java
@@ -6,10 +6,16 @@ package ${package}.frontend.controller;
 import org.apache.log4j.Logger;
 
 import ${package}.controller.ApplicationBaseSimpleController;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.vulpe.controller.annotations.Controller;
 import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
 
 @SuppressWarnings("serial")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Component("frontend.IndexController")
 @Controller(controllerType = ControllerType.FRONTEND)
 public class IndexController extends ApplicationBaseSimpleController {
 
