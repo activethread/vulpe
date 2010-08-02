4f9dc7b64977b34379892b84a81263b817112352
lordfelipe <lordfelipe@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2010-08-02 13:22:17 +0000

    Vulpe Site update

--- site/domains/src/main/java/org/vulpe/site/frontend/model/entity/Community.java
+++ site/domains/src/main/java/org/vulpe/site/frontend/model/entity/Community.java
@@ -1,6 +1,6 @@
 package org.vulpe.site.frontend.model.entity;
 
-import org.vulpe.model.entity.VulpeBaseSimpleEntity;
+import org.vulpe.model.entity.impl.VulpeBaseSimpleEntity;
 
 @SuppressWarnings("serial")
 public class Community extends VulpeBaseSimpleEntity {
