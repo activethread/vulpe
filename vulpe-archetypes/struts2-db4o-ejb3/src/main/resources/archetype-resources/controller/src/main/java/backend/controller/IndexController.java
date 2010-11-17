b6c2d3434196623666d5177e317968d4aea25a28
lordfelipe <lordfelipe@gmail.com>
2010-11-17 11:20:20 +0000

    Vulpe Archetypes new model

---
+++ vulpe-archetypes/struts2-db4o-ejb3/src/main/resources/archetype-resources/controller/src/main/java/backend/controller/IndexController.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.backend.controller;
+
+import org.apache.log4j.Logger;
+
+import ${package}.controller.ApplicationBaseSimpleController;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.vulpe.controller.annotations.Controller;
+import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
+
+@SuppressWarnings("serial")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Component("backend.IndexController")
+@Controller(type = ControllerType.BACKEND)
+public class IndexController extends ApplicationBaseSimpleController {
+
+	protected static final Logger LOG = Logger.getLogger(IndexController.class);
+
+}
---
+++ vulpe-archetypes/struts2-db4o-ejb3/src/main/resources/archetype-resources/web/src/main/webapp/WEB-INF/protected-jsp/backend/Index/Index.jsp
@@ -0,0 +1,5 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%@include file="/WEB-INF/protected-jsp/commons/common.jsp"%>
+<h2><fmt:message key='label.${rootArtifactId}.backend.Index.congratulations'/></h2>
