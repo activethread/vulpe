d0f42f9c0fbaadbab0d653d233f95d91a2893bc0
lordfelipe <lordfelipe@gmail.com>
2012-09-17 21:18:54 +0000

    Vulpe Archetypes 1.0.2.API3

---
+++ vulpe-archetypes/struts2-db4o-ejb3/src/main/resources/archetype-resources/controller/src/main/java/backend/controller/BackendController.java
@@ -0,0 +1,21 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.backend.controller;
+
+import ${package}.controller.ApplicationBaseController;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.vulpe.controller.annotations.Controller;
+import org.vulpe.controller.commons.VulpeControllerConfig.ControllerType;
+import org.vulpe.model.entity.impl.VulpeBaseSimpleEntity;
+
+@SuppressWarnings("serial")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Component("backend.IndexController")
+@Controller(type = ControllerType.BACKEND)
+public class IndexController extends ApplicationBaseController<VulpeBaseSimpleEntity, Long> {
+
+}
---
+++ vulpe-archetypes/struts2-db4o/src/main/resources/archetype-resources/domains/src/main/java/org/vulpe/config/domains/package-info.java
@@ -0,0 +1,8 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+@VulpeDomains(useDB4O = true)
+package org.vulpe.config.domains;
+
+import org.vulpe.config.annotations.VulpeDomains;
+
